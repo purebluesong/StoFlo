d20600ba6f2a6cd3628b44b2f9defeb89a403a7c
ylxdzsw <ylxdzsw@gmail.com>
2016-07-08 22:37:33 +0800

    Create basic layout

--- StoFlo_Web/create/Create.js
+++ StoFlo_Web/create/Create.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Paper from 'material-ui/Paper'
+import {Tabs, Tab} from 'material-ui/Tabs'
 import TextField from 'material-ui/TextField'
 import Login from '../common/Login'
 import GameWindow from '../common/GameWindow'
@@ -12,7 +13,7 @@ const styles = {
         float: 'left',
         width: '39vw',
         height: '50vh',
-        margin: '0 0 0 1vw',
+        margin: '.5vh .666vw 0 0',
         padding: '0 12px'
     }
 }
@@ -38,14 +39,28 @@ export default class Create extends React.Component {
         <MuiThemeProvider><div>
             <Login onFinished={this.onLoginFinished} />
             <GameChooser open={this.state.isChoosingGame} onFinished={this.onChooseGameFinished} />
-            <Paper style={styles.frameContainer}>
-                <TextField ref="textField" multiLine={true} fullWidth={true} rows={2}
-                           style={{ maxHeight: '50vh' }} textareaStyle={{ magin: '0', maxHeight: '50vh' }}
-                           hintText={'Input your story here.\nUse {{name}} to insert variables, and {{#}} to insert a breakpoint.'} />
+            <Paper style={{ width: '20vw', height: '100vh', float: 'left', marginRight: '.666vw' }}>
+                <Tabs>
+                    <Tab label="章节">
+
+                    </Tab>
+                    <Tab label="变量">
+
+                    </Tab>
+                    <Tab label="图片">
+
+                    </Tab>
+                </Tabs>
             </Paper>
-            <Paper style={styles.frameContainer}>
-                <GameWindow chapter={this.state.chapter} onAction={()=>{}} variables={{}} />
-            </Paper>
+            <div style={{ margin: '0 0 0 0' }}>
+                <Paper style={styles.frameContainer}>
+                    <TextField ref="textField" style={{ maxHeight: '50vh' }} textareaStyle={{ height: '47vh' }}
+                               multiLine={true} fullWidth={true} hintText={'Type your story here.'} />
+                </Paper>
+                <Paper style={styles.frameContainer}>
+                    <GameWindow chapter={this.state.chapter} onAction={()=>{}} variables={{}} />
+                </Paper>
+            </div>
         </div></MuiThemeProvider>
     )
 }
